5ed1b6ee3be39df623d2b046a16d10869feccb96
GURURAJ115 <gururajsnaik115@gmail.com>
2025-03-22 22:20:15 +0530

    Signup Signin Implemented for both admins and users

--- routes/admin.js
+++ routes/admin.js
@@ -1,17 +1,61 @@
 const {Router} = require("express");
-const adminRouter = Router();
 const {adminModel} = require("../db");
-adminRouter.post("/signup",(req,res)=>{
-    
-    res.json({
-        message: "endpoint"
-    })
+const bcrypt = require("bcrypt");
+const adminRouter = Router();
+const jwt = require("jsonwebtoken");
+const JWT_ADMIN_PASSWORD=process.env.JWT_ADMIN;
+
+adminRouter.post("/signup",async (req,res)=>{
+    const {email, password, firstName, lastName} = req.body;
+    const hashedPassword =await  bcrypt.hash(password,10);
+    try{
+        await adminModel.create({
+            email: email,
+            password: hashedPassword,
+            firstName: firstName,
+            lastName: lastName
+        })
+        res.json({
+            message: "success"
+        })
+    }
+    catch(error){
+        console.log("error"+error);
+        res.json({
+            message: "fucked"
+        })
+    }
 })
 
-adminRouter.post("/signin",(req,res)=>{
-    res.json({
-        message: "endpoint"
-    })
+adminRouter.post("/signin",async(req,res)=>{
+    const {email,password} = req.body;
+
+    try{
+        const admin = await adminModel.findOne({
+            email:email
+        })
+    
+        const isMatched = await bcrypt.compare(password, admin.password);
+    
+        if(!isMatched){
+            res.json({
+                message: "fcked"
+            })
+        }
+        else{
+            const token = jwt.sign({
+                id: admin._id
+            },JWT_ADMIN_PASSWORD)
+            res.json({
+                token: token
+            })
+        }
+    }catch(e){
+        console.log(e);
+        res.json({
+            message: "internal server error"
+        })
+    }
 })
 
 adminRouter.post("/course",(req,res)=>{
--- routes/user.js
+++ routes/user.js
@@ -1,16 +1,61 @@
 const {Router} = require("express");
-const userRouter = Router();
+const {userModel} = require("../db");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
-userRouter.post("/signup",(req,res)=>{
-    res.json({
-        message: "endpoint"
-    })
+const JWT_USER_PASSWORD=process.env.JWT_USER;
+const userRouter = Router();
+userRouter.post("/signup",async (req,res)=>{
+    const {email, password, firstName, lastName} = req.body;
+    const hashedPassword =await  bcrypt.hash(password,10);
+    try{
+        await userModel.create({
+            email: email,
+            password: hashedPassword,
+            firstName: firstName,
+            lastName: lastName
+        })
+        res.json({
+            message: "success"
+        })
+    }
+    catch(error){
+        console.log("error"+error);
+        res.json({
+            message: "fucked"
+        })
+    }
 })
 
-userRouter.post("/signin",(req,res)=>{
-    res.json({
-        message: "endpoint"
-    })
+userRouter.post("/signin",async(req,res)=>{
+    const {email,password} = req.body;
+
+    try{
+        const user = await userModel.findOne({
+            email:email
+        })
+    
+        const isMatched = await bcrypt.compare(password, user.password);
+    
+        if(!isMatched){
+            res.json({
+                message: "fcked"
+            })
+        }
+        else{
+            const token = jwt.sign({
+                id: user._id
+            },JWT_USER_PASSWORD)
+            res.json({
+                token: token
+            })
+        }
+    }catch(e){
+        console.log(e);
+        res.json({
+            message: "internal server error"
+        })
+    }
 })
 
 userRouter.get("/purchases",(req,res)=>{
